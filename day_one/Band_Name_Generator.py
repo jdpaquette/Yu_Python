03e2200929cdf66894cec596eba6b3e5c5d73b21
Joseph Paquette <josephdpaquette@gmail.com>
2025-02-15 21:41:52 -0500

    create day one project

---
+++ day_one/Band_Name_Generator.py
@@ -0,0 +1,6 @@
+# Band Name Generator Project - Day one
+# Created by Joseph Paquette
+print("Welcome to the Band Name Generator.")
+city = input("Enter city name:\n ")
+pet = input("Enter pet name:\n ")
+print("Your Band name is: " + city, pet)
